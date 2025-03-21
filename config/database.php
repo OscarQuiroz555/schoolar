cd6c788889ecc1dde3e2944408156e9bad7c980d
Oscar Quiroz <Oscarquiroz118@gmail.com>
2025-03-21 18:23:52 -0500

    Database config

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+//Config connection
+    $host       = "localhost"; //Parametros 
+    $port       = "5432";
+    $dbname     = "schoolar";
+    $user       = "postgres";
+    $password   = "POSTGRES";
+
+//Create connection 
+$conn = pg_connect("
+    host = $host
+    port = $port
+    dbname = $dbname 
+    user = $user
+    password = $password 
+");
+
+if(!$conn){
+    die("Connection error: " . pq_last_error()); //Validación
+}else{
+    echo "Success connection";
+}
+?>
