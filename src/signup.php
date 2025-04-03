370944331b2eb48e8e79ed19dd91917a5e1c6a40
Oscar Quiroz <Oscarquiroz118@gmail.com>
2025-04-03 11:39:14 -0500

    Signin: query and request

--- src/signup.php
+++ src/signup.php
@@ -1,5 +1,6 @@
 <?php
 
+    //credencial
     include('../config/database.php');
 
     $fname  = $_POST['f_name'];
@@ -7,15 +8,36 @@
     $email  = $_POST['e_mail'];
     $passwd = $_POST['passw'];
 
-    $sql = "INSERT INTO users (firstname, lastname, email, password)
-                VALUES('$fname','$lname',' $email','$passw') 
-    ";
+    // Para encriptar la contraseña (2 opciones)
+    //$enc_pass = md5($passwd);
+    $enc_pass = sha1($passwd);
 
-    $res = pg_query($conn, $sql);
+    //para que el correo email no se repita 
+    $sql_email_exist = 
+        "SELECT 
+            COUNT(email) as total 
+        FROM 
+            users 
+        WHERE 
+            email = '$email' 
+            LIMIT 1 ";
+    $res = pg_query($conn, $sql_email_exist);
 
     if($res){
-        echo "User has been created succesfully";
-    }else{
-        echo "Error";
+        $row = pg_fetch_assoc($res);
+        if($row['total'] > 0){
+            echo "Email alredy exists";
+        }else {
+            $sql = "INSERT INTO users (firstname, lastname, email, password)
+                VALUES('$fname', '$lname', '$email', '$enc_pass')";
+
+            $res = pg_query($conn, $sql);
+
+            if ($res){
+                echo "User has been created succesfully";
+            }else {
+                echo "Error";
+            }
+        }
     }
 ?>
