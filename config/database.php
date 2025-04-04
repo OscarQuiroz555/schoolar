fed62e959ab929e76248354c1e28a2e176e23cce
Oscar Quiroz <Oscarquiroz118@gmail.com>
2025-04-04 17:56:10 -0500

    connecting database to the supabase platform

--- config/database.php
+++ config/database.php
@@ -1,11 +1,20 @@
 <?php
+
+//Config connection
+    $host       = "aws-0-us-east-1.pooler.supabase.com"; //Parametros 
+    $port       = "6543";
+    $dbname     = "postgres";
+    $user       = "postgres.qabgjfrihttsynsygdhm";
+    $password   = "unicesmag@@";
+/*
+
 //Config connection
     $host       = "localhost"; //Parametros 
     $port       = "5432";
     $dbname     = "schoolar";
     $user       = "postgres";
     $password   = "POSTGRES";
-
+*/
 //Create connection 
 $conn = pg_connect("
     host = $host
