adf90b86be0dc7f5345452df6f29465715b6f0c4
Oscar Quiroz <oscarquiroz118@gmail.com>
2025-05-09 18:11:57 -0500

    request list users

--- src/list_users.php
+++ src/list_users.php
@@ -24,14 +24,38 @@
             <td>oscarquiroz118@mail.com</td>
             <td>Active</td>
             <td>
-                <img src = "icons/edit.png" width="15">
-                <img src = "icons/search.png" width="15">
-                <img src = "icons/trash.png" width="15">
+                <img src="icons/edit.png" width="15">
+                <img src="icons/search.png" width="15">
+                <img src="icons/trash.png" width="15">
             </td>
         </tr>
         <?php
-            //Here code
-            $sql = "SELECT";
+            $sql = "SELECT 
+                        firstname, 
+                        lastname, 
+                        email, 
+                        case when status = true then 'Active' else 'No Active' end as status
+                    FROM 
+                        users";
+            $res = pg_query($conn, $sql); 
+            if (!$res){
+                echo "Error query";
+                exit;
+            }
+
+            while ($row = pg_fetch_assoc($res)){
+                echo "<tr>";
+                echo "<td>" . $row['firstname'] . "</td>";
+                echo "<td>" . $row['lastname'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['status'] . "</td>";
+                echo "<td>";
+                echo "<a href=''><img src='icons/edit.png' width='15'></a>";
+                echo "<a href=''><img src='icons/search.png' width='15'></a>";
+                echo "<a href=''><img src='icons/trash.png' width='15'></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
         ?>
     </table>    
 </body>
